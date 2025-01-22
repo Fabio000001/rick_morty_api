f5dc9eee2247d428457cc1b5d8014dfe7ba74f47
Fabio Acosta <Fabio@Acosta.com>
2025-01-22 18:27:23 +0000

    Navbar y pages creadas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+        <div className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <Link className="btn btn-outline-primary" to="/">Inicio</Link>
+                <Link className="btn btn-outline-primary" to="/contacto">Contacto</Link>
+                <Link className="btn btn-outline-primary" to="/blog">Blog</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar
